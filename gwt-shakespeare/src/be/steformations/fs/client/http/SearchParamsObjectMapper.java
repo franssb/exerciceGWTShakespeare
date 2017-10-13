ed28a8f03da213e4467ea3473df5aad27f4a188b
java03 <java03@PRIM2014-22.fapse.priv>
2017-10-13 16:16:48 +0200

    recherche serveur affiche dans alert

---
+++ gwt-shakespeare/src/be/steformations/fs/client/http/SearchParamsObjectMapper.java
@@ -0,0 +1,8 @@
+package be.steformations.fs.client.http;
+
+import be.steformations.fs.client.event.search.SearchParams;
+
+public interface SearchParamsObjectMapper extends com.github.nmorel.gwtjackson.client.ObjectMapper<SearchParams>{
+
+	
+}
--- gwt-shakespeare/src/be/steformations/fs/client/http/Searcher.java
+++ gwt-shakespeare/src/be/steformations/fs/client/http/Searcher.java
@@ -1,24 +1,59 @@
 package be.steformations.fs.client.http;
 
+import org.eclipse.jetty.http.HttpParser.RequestHandler;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.Window;
 
 import be.steformations.fs.client.event.EventManager;
 import be.steformations.fs.client.event.search.SearchEvent;
 import be.steformations.fs.client.event.search.SearchEventManager;
 import be.steformations.fs.client.event.search.SearchParams;
 
-public class Searcher implements SearchEventManager{
+public class Searcher implements SearchEventManager, RequestCallback{
 
+	private RequestBuilder requestBuilder;
+	private SearchParamsObjectMapper mapper;
 	
 	public Searcher() {
 		super();
+		String url = Window.Location.getPath().replace("index.html", "");
+		url +="json/service";
+		this.requestBuilder = new RequestBuilder(RequestBuilder.POST, url);
+		this.requestBuilder.setCallback(this);
+		
+		
+		this.mapper = GWT.create(SearchParamsObjectMapper.class);
 		EventManager.getInstance().addHandler(SearchEvent.type, this);
 	}
 	
 	@Override
 	public void onSearch(SearchEvent event) {
 		SearchParams params = event.getParams();
-		GWT.log("Searcher.onSearch() =>" + params);		
+		GWT.log("Searcher.onSearch() =>" + params);	
+		String json = this.mapper.write(params);
+		this.requestBuilder.setRequestData(json);
+		try {
+			this.requestBuilder.send();
+		} catch (Exception e) {
+			Window.alert(e.getMessage());
+		}
+	}
+
+	@Override
+	public void onResponseReceived(Request request, Response response) {
+		GWT.log("Searcher.onResponseReceived()");
+		String monologue = response.getText();
+		Window.alert(monologue);
+	}
+
+	@Override
+	public void onError(Request request, Throwable exception) {
+		Window.alert(exception.getMessage());
 	}
 	
 }
